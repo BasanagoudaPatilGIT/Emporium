f14008ec91360cdcb9c39f7db0a83db2e05f9024
BasanagoudaPatil <basupatil71@gmail.com>
2019-05-12 12:01:03 +0530

    commited by Basu 12052019

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -8,15 +8,15 @@
 	}
 	
 	function validate_login_credentials() {
-		//$userName = "raghuram";
-		//$userPhoneno = "9611429415";
-		//$imei = 0;
-		//$userPassword = base64_encode("raghuram");
+		$userName = "raghuram";
+		$userPhoneno = "9611429415";
+		$imei = 358240051111110;
+		$userPassword = base64_encode("raghuram");
 		
-		$userName = $this->input->post('userName');
-		$userPhoneno = $this->input->post('userName');
-		$imei = $this->input->post('userIMEI');
-		$userPassword = base64_encode($this->input->post('userPassword'));
+		//$userName = $this->input->post('userName');
+		//$userPhoneno = $this->input->post('userName');
+		//$imei = $this->input->post('userIMEI');
+		//$userPassword = base64_encode($this->input->post('userPassword'));
 		
 		$query = $this->Login_model->validate($userName,$userPhoneno,$userPassword,$imei);
 		
@@ -71,6 +71,13 @@
 					'getReport'=>"Expense Report",
 					'mobiLogout' => "Logout"
 				);	
+				$dashboard_array[] = array(
+					'availableVersion'=>'v1.0.0',
+					'lowStockCount'=>2,
+					'inStockCount'=>5,
+				);
+				
+				
 				}else if($data['login']['index_name'] == 'Owner'){
 					$menu_array[] = array(
 					'purchaseProductDetails' => "Purchase",
@@ -89,6 +96,11 @@
 					'getReport' => "Expense Report",
 					'mobiLogout' => "Logout"
 				);	
+				$dashboard_array[] = array(
+					'availableVersion'=>'v1.0.0',
+					'lowStockCount'=>2,
+					'inStockCount'=>5,
+				);
 				}else if($data['login']['index_name'] == 'Customer') {
 					$menu_array[] = array(
 					'getProductDetails' => "Stock",
@@ -103,15 +115,18 @@
 					'getReport' => "Expense Report",
 					'mobiLogout' => "Logout"
 				);	
+					$dashboard_array[] = array(
+						'availableVersion'=>'v1.0.0',
+						'lowStockCount'=>2,
+						'inStockCount'=>5,
+					);
 				}
 					
 				$mobile_login_data[] = array(
 					'userDetails' => $user_array,
 					'menuDetails' => $menu_array,
-					'availableVersion'=>'v1.0.0',
-					'lowStockCount'=>2,
-					'inStockCount'=>5,
-					'mainMenuDetails' => $main_menu_array
+					'mainMenuDetails' => $main_menu_array,
+					'dashboardDetails' => $dashboard_array,
 				);
 				
 				print_r(json_encode($mobile_login_data));
@@ -125,6 +140,102 @@
 			print_r(json_encode($login_failed_data));	
 			//return $login_failed_data;
 		}
+	}
+	
+	function reloadHome() {
+		//$userId = $this->input->post('userId');
+		$userId =1;
+		$data['login'] = $this->Login_model->get_user_detail_by_userId($userId);
+		$user_array[] = array(
+			'userId' => $data['login']['id'],
+			'entCode' => $data['login']['ent_code'],
+			'name' => $data['login']['user_full_name'],
+			'mobileNo' => $data['login']['user_phone_no'],
+			'emailId' => $data['login']['user_email_id'],
+			'imageName' => $data['login']['user_image'],
+			'designationIndex' => $data['login']['user_designation_index'],
+			'designation' => $data['login']['index_name'],
+			'userAddress' => $data['login']['user_address'],
+			'userFullName'=>$data['login']['user_full_name'],
+			'userAddress'=>$data['login']['user_address'],
+			'userPhoneNo'=>$data['login']['user_phone_no'],
+			'flatNo'=>$data['login']['flat_no'],
+			'wing'=>$data['login']['wing'],
+			'apartmentName'=>$data['login']['apartment_name'],		
+		);
+		if($data['login']['index_name'] == 'Admin'){
+		$menu_array[] = array(
+			'entityDetails' => "Entity",
+			'AddCategoryOrSubCategory' => "Add CategorySubCategory",
+		);
+		
+		$main_menu_array[] = array(
+			'getProductDetails' => "Stock",
+			'invoiceDetails' => "Invoice",
+			'getReport'=>"Expense Report",
+			'mobiLogout' => "Logout"
+		);	
+		$dashboard_array[] = array(
+			'availableVersion'=>'v1.0.0',
+			'lowStockCount'=>2,
+			'inStockCount'=>5,
+		);
+		
+		
+		}else if($data['login']['index_name'] == 'Owner'){
+			$menu_array[] = array(
+			'purchaseProductDetails' => "Purchase",
+			'getProductDetails' => "Stock",
+			'orderDetails' => "Orders",
+			'invoiceDetails' => "Invoice",
+		);
+		
+		$main_menu_array[] = array(
+			'purchaseProductDetails' => "Purchase",
+			'getProductDetails' => "Stock",
+			'orderDetails' => "Orders",
+			'invoiceDetails' => "Invoice",
+			'loadUploadProducts' => "Upload Products",
+			'employeeDetails' => "Employee",
+			'getReport' => "Expense Report",
+			'mobiLogout' => "Logout"
+		);	
+		$dashboard_array[] = array(
+			'availableVersion'=>'v1.0.0',
+			'lowStockCount'=>2,
+			'inStockCount'=>5,
+		);
+		}else if($data['login']['index_name'] == 'Customer') {
+			$menu_array[] = array(
+			'getProductDetails' => "Stock",
+			'orderDetails' => "Orders",
+			'invoiceDetails' => "Invoice",
+			'getReport' => "Expence Report",
+			);
+			$main_menu_array[] = array(
+			'getProductDetails' => "Stock",
+			'orderDetails' => "Orders",
+			'invoiceDetails' => "Invoice",
+			'getReport' => "Expense Report",
+			'mobiLogout' => "Logout"
+		);	
+			$dashboard_array[] = array(
+				'availableVersion'=>'v1.0.0',
+				'lowStockCount'=>2,
+				'inStockCount'=>5,
+			);
+		}
+			
+		$mobile_login_data[] = array(
+			'userDetails' => $user_array,
+			'menuDetails' => $menu_array,
+			'dashboardDetails' => $dashboard_array,
+			'mainMenuDetails' => $main_menu_array
+		);
+		
+		print_r(json_encode($mobile_login_data));
+
+		
 	}
 	
         public function mobiLogout()
--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -50,6 +50,17 @@ class Login_model extends CI_Model
 		return $query->row_array();
 	}
 	
+	public function get_user_detail_by_userId($userId)
+	{
+		$sql= "select u.*,i.index_name,e.ent_name,e.ent_code,fl.flat_no,w.wing,a.apartment_name from tab_user as u 
+		left join tab_index as i on (i.index_id = u.user_designation_index) left join tab_entity as e on (e.ent_code = u.ent_code)
+		left join tab_flat_no as fl on (fl.id = u.user_flat_id) left join tab_wing as w on (w.id = fl.wing_id)	
+		left join tab_apartment as a on (a.id = w.apartment_id) where u.id= ? ";
+		$query = $this->db->query($sql,array($userId));
+		
+		return $query->row_array();
+	}
+	
 	public function send_mail($email_id)
 	{
 		$this->db->select('r.*,d.designation');
